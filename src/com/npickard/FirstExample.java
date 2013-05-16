9bce6eda9c74fbab9dd37027c37b3e30d3d1cd84
nigel <nigel_pickard@yahoo.com>
2013-05-16 14:02:01 -0400

    initial commit -working on the configuration hbm xml file

---
+++ src/com/npickard/FirstExample.java
@@ -0,0 +1,36 @@
+package com.npickard;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class FirstExample {
+	public static void main(String[] args) {
+		Session session = null;
+
+		try {
+			// This step will read hibernate.cfg.xml and prepare hibernate for
+			// use
+			SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+			session = sessionFactory.openSession();
+			// Create new instance of Contact and set values in it by reading
+			// them from form object
+			System.out.println("Inserting Record");
+			Contact contact = new Contact();
+			contact.setId(3);
+			contact.setFirstName("Deepak");
+			contact.setLastName("Kumar");
+			contact.setEmail("deepak_38@yahoo.com");
+			session.save(contact);
+			System.out.println("Done");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			// Actual contact insertion will happen at this step
+			session.flush();
+			session.close();
+
+		}
+
+	}
+}
